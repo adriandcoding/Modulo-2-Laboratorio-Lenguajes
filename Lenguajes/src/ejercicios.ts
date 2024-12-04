21bdca5c8b8676e4ae004b0badf17f1cb342cafc
adriandcoding <theamr91@gmail.com>
2024-12-04 07:56:19 +0100

    Add TreeNode type definition in TypeScript to represent hierarchical structures; include comment explaining trees and nodes

--- Lenguajes/src/ejercicios.ts
+++ Lenguajes/src/ejercicios.ts
@@ -286,3 +286,12 @@ console.log(memoizedGreet(3, "chun")); // chun chun chun
 console.log(memoizedGreet(1, "pam")); // pam
 console.log(memoizedGreet(3, "chun")); // chun chun chun
 console.log(count); // 2
+
+/* ¿Cómo generarías con TypeScript un tipado para estructuras en forma de árbol?
+Un árbol es una estructura que parte de un nodo raíz, a partir del cual salen más nodos. Cada nodo en un árbol puede tener hijos (más nodos) o no tenerlos (convirtiéndose en un nodo final o una "hoja"). */
+
+type TreeNode = {
+  value: string;
+  children: TreeNode[];
+  parent?: TreeNode;
+};
